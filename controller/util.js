cc199ca8c957ad9d73c28ca7b74554124bd72241
nodemirai <wuyunfeng0@gmail.com>
2017-12-14 20:09:59 +0800

    静态资源路径填写之config文件中

--- controller/util.js
+++ controller/util.js
@@ -1,6 +1,7 @@
 /**
  * 工具接口
  */
+import config from '../config'
 
 import fs from 'fs'
 
@@ -11,7 +12,7 @@ const UtilController = {
     console.log('原始文件名：%s', file.originalname);
     console.log('文件大小：%s', file.size);
     console.log('文件保存路径：%s', file.path);
-    fs.writeFile('static/img/' + file.originalname, file.buffer, (err) => {
+    fs.writeFile(config.static + file.originalname, file.buffer, (err) => {
       if (err) throw err
       res.send({ ret_code: '0' });
     })
