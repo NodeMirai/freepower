13a1684e359963553726b5ad1130f9cf8f8282ad
nodemirai <wuyunfeng0@gmail.com>
2018-06-15 17:44:44 +0800

    1. add: .gitignore中添加access.log,git仓储不保存日志文件

--- routes/index.js
+++ routes/index.js
@@ -8,31 +8,26 @@ import Router from 'koa-router'
 const router = new Router()
 
 async function authenticate(ctx, next) {
-  console.log('hehe----------------')
-  /* console.log(req.path)
+  
     //  从请求中获取token
-  var token = req.headers['authorization']
+  var token = ctx.headers['authorization']
     // 校验token有效性
   if (token) {
     jwt.verify(token, config.secret, (err, decoded) => {
       if (err) {
-        return res.status(403).send({
-          status: 403,
-          message: 'Failed to authenticate token.'
-        })
+        ctx.status = 403
+        ctx.message = 'Failed to authenticate token.'
       } else {
         // token校验正确,保存decoded传递给其他路由使用
-        req.decoded = decoded
+        ctx.state.decoded = decoded
         next()
       }
     })
   } else {
     // 不存在token，返回403
-    return res.status(403).send({
-      status: 403,
-      message: 'No token provided'
-    })
-  } */
+    ctx.status = 403
+    ctx.message = 'No token provided'
+  }
 
 }
 
